d78d261a65273d889546e4a2ee62d764dcaa01e0
vinimaffra03 <viniciushmdem15@gmail.com>
2026-01-21 19:03:35 -0300

    section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import Card from "@/components/home/card";
-import { DEPLOY_URL } from "@/lib/constants";
-import { Github } from "@/components/shared/icons";
 import WebVitals from "@/components/home/web-vitals";
 import Image from "next/image";
 
@@ -33,33 +31,18 @@ export default async function Home() {
         >
           <a
             className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href={DEPLOY_URL}
+            href="https://github.com/vinimaffra03"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg
-              className="h-4 w-4 group-hover:text-black"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 4L20 20H4L12 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
             <p>Entrar na comunidade</p>
           </a>
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/steven-tey/precedent"
+            href="https://github.com/vinimaffra03"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Github />
             <p>
               <span className="hidden sm:inline-block">Conhecer Primeiro</span>
             </p>
